9f85c485cab98fa0df34523b2002e30d4d3978fe
Sandeep Suresh <sureshsa@usc.edu>
2020-03-24 23:45:33 -0700

    I created the styles folder and different files to place all your styles in. The idea behind this is anything that fall under these categories should be added here. Export styles as needed for each screen.

---
+++ styles/text.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  title: {
+    margin: 20,
+    color: "#FFFFFF",
+    fontStyle: "italic",
+    fontWeight: "900",
+    fontSize: 60
+  }
+});
