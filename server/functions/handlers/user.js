424a19fd26935b2cec284cd01764a55b3bc97868
Sandeep Suresh <sureshsa@usc.edu>
2020-05-19 11:48:39 -0700

    Created the login endpoint and I'm about to test it on the client.

--- server/functions/handlers/user.js
+++ server/functions/handlers/user.js
@@ -1,11 +1,13 @@
 //This file serves as a place to implement all cloud functions
 //The cloud functions are then exported and given a route in index.js
 const { db, admin } = require("../util/admin");
+const config = require("../util/config");
 
 exports.helloWorld = (req, res) => {
   res.send("Hello world!");
 };
 
+//Function to get all live tutors
 exports.getTutors = (req, res) => {
   db.collection("tutors")
     .where("isLive", "==", true)
@@ -23,5 +25,72 @@ exports.getTutors = (req, res) => {
     .catch((error) => {
       res.status(500).json({ error: `Error getting all tutors` });
       console.error("Error: " + error);
+    });
+};
+
+//Function to login user with email and password
+exports.login = (req, res) => {
+  //User data should just email, password, account type can be determined
+  //TODO: Login in to specific accounts if both student and tutors
+
+  //Credentials from the body of the request
+  let creds = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  //TODO: Check for any invalid email/password syntax
+
+  //TODO: Jump into login flow
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(creds.email, creds.password)
+    .then((user) => {
+      const uid = user.user.uid;
+      firebase
+        .firestore()
+        .collection("students")
+        .doc(uid)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            userUid = uid;
+            //Return the user info to update redux on the client
+            const currUser = {
+              uid: user.user.uid,
+              email: user.user.email,
+              type: "student",
+              userData: doc.data(),
+            };
+            return res.status(200).json(currUser);
+          } else {
+            firebase
+              .firestore()
+              .collection("tutors")
+              .doc(uid)
+              .get()
+              .then((doc) => {
+                if (doc.exists) {
+                  userUid = uid;
+                  //Return the user info to update redux on the client
+                  const currUser = {
+                    uid: user.user.uid,
+                    email: user.user.email,
+                    type: "tutor",
+                    userData: doc.data(),
+                  };
+                  return res.status(200).json(currUser);
+                } else {
+                  console.log("Error");
+                }
+              });
+          }
+        });
+    })
+    .catch((error) => {
+      //Print the error code and just return to client
+      //All logic of wrong email, bad password can be done on client
+      console.log(error.code);
+      return res.status(403).json(error);
     });
 };
--- server/functions/index.js
+++ server/functions/index.js
@@ -3,13 +3,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors")({ origin: true });
 
-const { helloWorld, getTutors } = require("./handlers/user");
+const { helloWorld, getTutors, login } = require("./handlers/user");
 
 app.use(cors);
 
 //Routes for all functions
 app.get("/helloWorld", helloWorld);
 app.get("/liveTutors", getTutors); //Grab all tutors who are live
+app.get("/login", login);
 
 //Deploying all routes in the app to functions in firebase
 exports.api = functions.https.onRequest(app);
