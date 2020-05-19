2616b4ec121dac3c338a3439a1e70fa2a0f7b731
Sandeep Suresh <sureshsa@usc.edu>
2020-05-19 15:12:00 -0700

    Minor changes to get log in working. Still have some work to do here.

--- App.js
+++ App.js
@@ -4,27 +4,32 @@ import { decode, encode } from "base-64";
 import "./screens/components/global";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import axios from "axios";
 global.crypto = require("@firebase/firestore");
 global.crypto.getRandomValues = (byteArray) => {
-  for (let i = 0; i < byteArray.length; i++) {
-    byteArray[i] = Math.floor(256 * Math.random());
-  }
+    for (let i = 0; i < byteArray.length; i++) {
+        byteArray[i] = Math.floor(256 * Math.random());
+    }
 };
 
 if (!global.btoa) {
-  global.btoa = encode;
+    global.btoa = encode;
 }
 
 if (!global.atob) {
-  global.atob = decode;
+    global.atob = decode;
 }
 
+//Setting the baseURL for our cloud functions endpoint
+axios.defaults.baseURL =
+    "https://us-central1-lavalab-project.cloudfunctions.net/api";
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <SwitchNavigator screenProps={{ test: "test" }} />
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <SwitchNavigator screenProps={{ test: "test" }} />
+            </Provider>
+        );
+    }
 }
